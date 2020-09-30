d65c1516402eeb73f67c40b43c587dc30ad2ba81
neveSZ <vnsilva2000@gmail.com>
2020-09-30 16:58:25 -0300

    Adicionado testes de matricula Co-authored-by: Ueslei Neves <unp1210@gmail.com>

--- spec/features/client_register_subsidiary_spec.rb
+++ spec/features/client_register_subsidiary_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 feature 'client enroll a subsidiary' do
-  scenario 'sucessfully logged in' do
-    client = create(:client)
+  scenario 'and view details summary' do
+    client = create(:client, name: '', cpf: '', address: '')
     subsidiary = Subsidiary.new(id: 1, name: 'Vila Maria', address: 'Avenida Osvaldo Reis, 801', cep: '88306-773')
     allow(Subsidiary).to receive(:all).and_return([subsidiary])
 
@@ -10,6 +10,9 @@ feature 'client enroll a subsidiary' do
     visit root_path
     click_on subsidiary.name
     click_on 'Matricule-se agora'
+    fill_in 'Nome', with: 'João'
+    fill_in 'CPF', with: '753.983.030-15'
+    fill_in 'Endereço', with: 'Rua Salvado Simoes'
     select 'Black', from: 'Selecione seu plano'
     select 'Boleto', from: 'Forma de pagamento'
     click_on 'Próximo'
@@ -22,17 +25,62 @@ feature 'client enroll a subsidiary' do
     expect(page).to have_content('Boleto')
     expect(page).to have_content('Permanncia minima: 12 meses')
     expect(page).to have_content('59,90')
-    expect(page).to have_content('Confirmar')
-    expect(page).to have_content('Voltar')
+    expect(page).to have_link('Confirmar')
+    expect(page).to have_link('Voltar')
     # Plano: preco mensal, nome, permanencia minima, status
   end
 
-  xscenario 'not logged in' do
+  scenario 'sucessfully' do
+    client = create(:client, name: '', cpf: '', address: '')
+    subsidiary = Subsidiary.new(id: 1, name: 'Vila Maria', address: 'Avenida Osvaldo Reis, 801', cep: '88306-773')
+    allow(Subsidiary).to receive(:all).and_return([subsidiary])
+
+    login_as(client)
+    visit root_path
+    click_on subsidiary.name
+    click_on 'Matricule-se agora'
+    fill_in 'Nome', with: 'João'
+    fill_in 'CPF', with: '753.983.030-15'
+    fill_in 'Endereço', with: 'Rua Salvado Simoes'
+    select 'Black', from: 'Selecione seu plano'
+    select 'Boleto', from: 'Forma de pagamento'
+    click_on 'Próximo'
+    click_on 'Confirmar'
+
+    expect(page).to have_content('Matricula realizada com sucesso')
   end
 
-  xscenario 'not must fill in blank' do
+  scenario 'not must fill in blank' do
+    client = create(:client, name: '', cpf: '', address: '')
+    subsidiary = Subsidiary.new(id: 1, name: 'Vila Maria', address: 'Avenida Osvaldo Reis, 801', cep: '88306-773')
+    allow(Subsidiary).to receive(:all).and_return([subsidiary])
+
+    login_as(client)
+    visit root_path
+    click_on subsidiary.name
+    click_on 'Matricule-se agora'
+    click_on 'Próximo'
+
+    expect(page).to have_content('não pode ficar em branco', count: 5)
   end
 
-  xscenario 'already enrolled' do
+  scenario 'and return to form register' do
+    client = create(:client, name: '', cpf: '', address: '')
+    subsidiary = Subsidiary.new(id: 1, name: 'Vila Maria', address: 'Avenida Osvaldo Reis, 801', cep: '88306-773')
+    allow(Subsidiary).to receive(:all).and_return([subsidiary])
+
+    login_as(client)
+    visit root_path
+    click_on subsidiary.name
+    click_on 'Matricule-se agora'
+    fill_in 'Nome', with: 'João'
+    fill_in 'CPF', with: '753.983.030-15'
+    fill_in 'Endereço', with: 'Rua Salvado Simoes'
+    select 'Black', from: 'Selecione seu plano'
+    select 'Boleto', from: 'Forma de pagamento'
+    click_on 'Próximo'
+    click_on 'Voltar'
+
+    expect(current_path).to eq new_enroll_path(subsidiary.id)
   end
 end
