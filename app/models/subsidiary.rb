ec5bde4e0e0d0df431921fc15bccabccf645b767
neveSZ <vnsilva2000@gmail.com>
2020-09-30 16:09:17 -0300

    Correção de indentação e adicionado objetos no model subsidiary para testes Co-authored-by: Ueslei Neves <unp1210@gmail.com>

--- app/models/subsidiary.rb
+++ app/models/subsidiary.rb
@@ -9,6 +9,8 @@ class Subsidiary
   end
 
   def self.all
-    []
+    [new(id: 1, name: 'Vila Maria', address: 'Avenida Osvaldo Reis, 801', cep: '88306-773'),
+     new(id: 2, name: 'Ipiranga', address: 'Rua da Concórdia, 201', cep: '57071-812'),
+     new(id: 3, name: 'Santos', address: 'Rua das Hortências, 302', cep: '78150-384')]
   end
 end
