1de20a7b57a76657cf598102231f5114fe0b0ec1
Lucas Perez <lucastperez@gmail.com>
2020-10-06 12:39:46 -0300

    Testes unitarios personal

--- spec/models/personal_spec.rb
+++ spec/models/personal_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Personal, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  it 'Name, CREF, CPF, Email and Password cannot be blank' do
+    personal = Personal.create
+
+    expect(personal.errors[:name]).to include('não pode ficar em branco')
+    expect(personal.errors[:cref]).to include('não pode ficar em branco')
+    expect(personal.errors[:cpf]).to include('não pode ficar em branco')
+    expect(personal.errors[:email]).to include('não pode ficar em branco')
+    expect(personal.errors[:password]).to include('não pode ficar em branco')
+  end
+
+  it 'CPF and CREF must be valid' do
+    personal = Personal.create(cpf: '12345', cref: '67890')
+
+    expect(personal.errors[:cref]).to include('não é válido')
+    expect(personal.errors[:cpf]).to include('não é válido')
+  end
 end
