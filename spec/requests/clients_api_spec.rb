4c28a273fbd7590ead7bd3b02cf175b50328f2af
Paulo Rodrigues <paulor2222@gmail.com>
2020-10-08 16:25:56 -0300

    Client api get /api/v1/clients/:id
    Co-authored-by Reinaldo reinaldolluiz@gmail.com

--- spec/requests/clients_api_spec.rb
+++ spec/requests/clients_api_spec.rb
@@ -13,5 +13,32 @@ describe 'Clients API' do
       expect(response.body).to include(clients[1].cpf)
       expect(response.body).to include(clients[1].email)
     end
+
+    it 'returns []' do
+      get '/api/v1/clients'
+      
+      response_json = JSON.parse(response.body)
+
+      expect(response).to have_http_status(200)
+      expect(response.content_type).to include('application/json')
+      expect(response_json).to be_empty
+    end
+  end
+
+  context 'GET /api/v1/client/:id' do
+    context 'Record exists' do
+      let(:client) { create(:client) }
+
+      it 'returns client' do
+        get "/api/v1/clients/#{client.id}"
+
+        response_json = JSON.parse(response.body)
+
+        expect(response).to have_http_status(200)
+        expect(json_response).to include(client.cpf)
+        expect(json_response).to include(client.email)
+      end
+    end
+
   end
 end
