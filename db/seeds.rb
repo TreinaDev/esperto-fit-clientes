b30914806581709fba975daf731872f5e218423a
Paulo Rodrigues <paulor2222@gmail.com>
2020-10-08 17:11:14 -0300

    seed clients
    Co-authored-by Reinaldo reinaldolluiz@gmail.com

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p 'seeding'
+
+10.times do |i|
+  Client.create!(
+    email: "test#{i}@email.com",
+    password: 'password',
+    cpf: CPF.generate(formatted: true)
+  )
+end
+
+p 'done!'
