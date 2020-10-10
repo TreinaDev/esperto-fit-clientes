9f4b35997cc23491fca48f71f8655fb9fd8a3778
Paulo Rodrigues <paulor2222@gmail.com>
2020-10-09 22:42:10 -0300

    managed rubocop offenses
    Co-authored-by Anthony anthonynakamoto@gmail.com

--- spec/models/personal_spec.rb
+++ spec/models/personal_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Personal, type: :model do
   context 'associations' do
-    it {is_expected.to have_many(:appointments).dependent(:destroy)}
+    it { is_expected.to have_many(:appointments).dependent(:destroy) }
   end
 
   it 'Name, CREF, CPF, Email and Password cannot be blank' do
