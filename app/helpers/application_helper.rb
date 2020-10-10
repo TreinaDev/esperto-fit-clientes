c73c5a7f7dfa4322493752b770ec9efb53792a71
Paulo Rodrigues <paulor2222@gmail.com>
2020-10-10 12:41:21 -0300

    removed helper method owner? and method owner? to the model
    Co-authored-by Anthony anthonynakamoto@gmail.com

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def owner?(appointment)
-    appointment.personal == current_personal
-  end
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,8 @@ class Appointment < ApplicationRecord
   belongs_to :personal
 
   validates :personal, :appointment_date, :price_per_hour, presence: true
+
+  def owner?(current_personal)
+    personal == current_personal
+  end
 end
