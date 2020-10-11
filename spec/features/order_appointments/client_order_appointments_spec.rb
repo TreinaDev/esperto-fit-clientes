7f14c11abc3bfce2ad0ca5d45c71237c07053086
Paulo Rodrigues <paulor2222@gmail.com>
2020-10-11 16:07:43 -0300

    localize attributes for appointments

--- spec/features/order_appointments/client_order_appointments_spec.rb
+++ spec/features/order_appointments/client_order_appointments_spec.rb
@@ -9,10 +9,26 @@ feature 'Order Appointments' do
     visit root_path
     click_link 'Personais disponíveis'
 
-    expect(page).to have_content(appointments[0].appointment_date)
-    expect(page).to have_content(appointments[1].appointment_date)
+    expect(page).to have_content(appointments[0].appointment_date.strftime('%d/%m/%Y'))
+    expect(page).to have_content(appointments[1].appointment_date.strftime('%d/%m/%Y'))
     expect(page).to have_content(appointments[1].appointment_time)
     expect(page).to have_content(appointments[1].appointment_time)
+  end
+
+  scenario 'Client view all their ordered appointments' do
+    client = create(:client)
+    login_as(client, scope: :client)
+    appointments = create_list(:appointment, 2)
+    other_appointment = create(:appointment, appointment_date: Time.zone.tomorrow)
+    create(:order_appointment, client: client, appointment: appointments[0])
+    create(:order_appointment, client: client, appointment: appointments[1])
+
+    visit root_path
+    click_link 'Treinos agendados'
+
+    expect(page).to have_content(appointments[0].appointment_date.strftime('%d/%m/%Y'))
+    expect(page).to have_content(appointments[1].appointment_date.strftime('%d/%m/%Y'))
+    expect(page).not_to have_content(other_appointment.appointment_date.strftime('%d/%m/%Y'))
   end
 
   scenario 'only available appointments' do
@@ -24,9 +40,9 @@ feature 'Order Appointments' do
     visit root_path
     click_link 'Personais disponíveis'
 
-    expect(page).to have_content(appointments[0].appointment_date)
-    expect(page).to have_content(appointments[1].appointment_date)
-    expect(page).not_to have_content(not_available.appointment_date)
+    expect(page).to have_content(appointments[0].appointment_date.strftime('%d/%m/%Y'))
+    expect(page).to have_content(appointments[1].appointment_date.strftime('%d/%m/%Y'))
+    expect(page).not_to have_content(not_available.appointment_date.strftime('%d/%m/%Y'))
   end
 
   scenario 'view details' do
