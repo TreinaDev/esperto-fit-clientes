566a53bd8df571b2bce4987630cb58c9dbb7d997
Paulo Rodrigues <paulor2222@gmail.com>
2020-10-13 09:19:49 -0300

    personal cancel appointment more test cases

--- spec/features/order_appointments/personal_cancel_order_appointment_spec.rb
+++ spec/features/order_appointments/personal_cancel_order_appointment_spec.rb
@@ -1,6 +1,29 @@
 require 'rails_helper'
 
 feature 'Personal cancel order appointment' do
+  scenario 'need to be owner' do
+    personal = create(:personal)
+    appointment = create(:appointment)
+    create(:order_appointment, appointment: appointment)
+
+    visit appointment_path(appointment)
+
+    expect(page).not_to have_link('Cancelar agendamento')
+  end
+
+  scenario 'have link' do
+    personal = create(:personal)
+    login_as(personal, scope: :personal)
+    appointment = create(:appointment, personal: personal, status: 'ordered')
+    create(:order_appointment, appointment: appointment)
+
+    visit root_path
+    click_link 'Minha Agenda'
+    click_link 'Detalhes'
+
+    expect(page).to have_link('Cancelar agendamento', href: cancel_appointment_path(appointment))
+  end
+
   scenario 'successfully' do
     personal = create(:personal)
     login_as(personal, scope: :personal)
