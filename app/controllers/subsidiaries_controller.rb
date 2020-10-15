2d90db3223b409c34bd97c7f0d41214a0b2d5cc4
Lucas Perez <lucastperez@gmail.com>
2020-10-14 22:57:37 -0300

    Adicionados breadcrumbs no controller de filiais

--- app/controllers/subsidiaries_controller.rb
+++ app/controllers/subsidiaries_controller.rb
@@ -1,9 +1,11 @@
 class SubsidiariesController < ApplicationController
   def show
     @subsidiary = Subsidiary.find(params[:id])
+    add_breadcrumb(@subsidiary.name, subsidiary_path)
   end
 
   def search
+    add_breadcrumb('Busca')
     @subsidiaries = Subsidiary.search(params[:q])
   end
 end
