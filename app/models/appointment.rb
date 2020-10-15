00b19f7e54c6deb99157c0f2e90178b2da7440a5
neveSZ <vnsilva2000@gmail.com>
2020-10-15 17:10:54 -0300

    WIP: edicao de agenda do personal e validacoes
    Co-authored-by: Lucas Perez <lucastperez@gmail.com>

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,6 +5,7 @@ class Appointment < ApplicationRecord
   enum status: { available: 0, ordered: 10, canceled: 20 }
 
   validates :personal, :appointment_date, :price_per_hour, presence: true
+  validate :appointment_date, on_or_before: Date.current
 
   def owner?(current_personal)
     personal == current_personal
